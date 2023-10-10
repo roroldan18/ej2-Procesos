5770e4b2986a7ff46045fc1a29ac38a4423bb5a0
Rodrigo Roldán <rodrigo_roldan18@hotmail.com>
2023-10-11 00:39:18 +0200

    Primer Commit

---
+++ src/Calculadora.java
@@ -0,0 +1,37 @@
+/**
+ * Calculadora
+ *
+ * Argumentos
+ * arg[0] = operacion
+ * arg[1] = operando1
+ * arg[2] = operando2
+ * arg[3] = ruta de salida
+ *
+ * Autor: Rodrigo Roldán
+ */
+
+public class Calculadora {
+    public static void main(String[] args) {
+
+        // Controlar argumentos que recibe
+        if(args.length != 4) {
+            System.out.println("Error: argumentos inválidos");
+            System.out.println("Uso: java Calculadora <operacion> <operando1> <operando2> <ruta de salida>");
+            System.exit(1);
+        }
+
+        // Verificar que los operandos sean números
+        
+
+        // Verificar la operación que se va a hacer, si el símbolo es correcto
+
+
+
+        // Hacer la operación
+
+        // Imprimirla en pantalla
+
+        // Guardarla en un archivo
+
+    }
+}
